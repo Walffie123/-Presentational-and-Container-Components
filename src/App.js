4d61de5a5b56322e7ff6d9b992adccea64490792
MaZuongToiChoi <lamthdde160172@fpt.edu.vn>
2022-10-05 13:10:08 +0700

    Configuring React

--- src/App.js
+++ src/App.js
@@ -22,4 +22,32 @@ function App() {
   );
 }
 
+import { Navbar, NavbarBrand } from 'reactstrap';
+
+class App extends Component {
+
+render() {
+
+return (
+
+<div className="App">
+
+<Navbar dark color="primary">
+
+<div className="container">
+
+<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+
+</div>
+
+</Navbar>
+
+</div>
+
+);
+
+}
+
+}
+
 export default App;
