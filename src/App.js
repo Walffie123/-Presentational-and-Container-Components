da6b139e29970d6bc02f0104fcdf368608fe323b
MaZuongToiChoi <lamthdde160172@fpt.edu.vn>
2022-10-10 21:52:02 +0700

    Presentational and Container Components

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Main from './components/MainComponent';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  class App extends Component {
 
-import { Navbar, NavbarBrand } from 'reactstrap';
 
-class App extends Component {
+    render() {
 
-render() {
+      return (
 
-return (
+        <div >
+          <Main />
+        </div>
 
-<div className="App">
+      );
 
-<Navbar dark color="primary">
+    }
 
-<div className="container">
-
-<NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-
-</div>
-
-</Navbar>
-
-</div>
-
-);
-
-}
-
-}
+  }
 
 export default App;
